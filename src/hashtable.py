e4012003484b23dee268e38975ee01b7d1049b1c
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-12-02 15:18:14 -0700

    now able to insert hashed key into array

--- src/hashtable.py
+++ src/hashtable.py
@@ -51,7 +51,15 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+
+        hashed_key = self._hash_mod(key)
+
+        if not self.storage[hashed_key]:
+            self.storage[hashed_key] = value
+            print(self.storage)
+            return
+        else:
+            print('index is being used')
 
 
 
@@ -88,30 +96,36 @@ class HashTable:
 
 
 
-if __name__ == "__main__":
-    ht = HashTable(2)
+# if __name__ == "__main__":
+#     ht = HashTable(2)
 
-    ht.insert("line_1", "Tiny hash table")
-    ht.insert("line_2", "Filled beyond capacity")
-    ht.insert("line_3", "Linked list saves the day!")
+#     ht.insert("line_1", "Tiny hash table")
+#     ht.insert("line_2", "Filled beyond capacity")
+#     ht.insert("line_3", "Linked list saves the day!")
 
-    print("")
+#     print("")
 
-    # Test storing beyond capacity
-    print(ht.retrieve("line_1"))
-    print(ht.retrieve("line_2"))
-    print(ht.retrieve("line_3"))
+#     # Test storing beyond capacity
+#     print(ht.retrieve("line_1"))
+#     print(ht.retrieve("line_2"))
+#     print(ht.retrieve("line_3"))
 
-    # Test resizing
-    old_capacity = len(ht.storage)
-    ht.resize()
-    new_capacity = len(ht.storage)
+#     # Test resizing
+#     old_capacity = len(ht.storage)
+#     ht.resize()
+#     new_capacity = len(ht.storage)
 
-    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+#     print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
-    # Test if data intact after resizing
-    print(ht.retrieve("line_1"))
-    print(ht.retrieve("line_2"))
-    print(ht.retrieve("line_3"))
+#     # Test if data intact after resizing
+#     print(ht.retrieve("line_1"))
+#     print(ht.retrieve("line_2"))
+#     print(ht.retrieve("line_3"))
 
-    print("")
+#     print("")
+
+def hash_test():
+    HashTable(2).insert('abc', 'value')
+    HashTable(5).insert('abc', 'value')
+
+print(hash_test())
